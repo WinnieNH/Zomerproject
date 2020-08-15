5a5a6dae394598051f06c58577a8a3ef04aabd9f
WinnieNH <winniehoogakker@gmail.com>
2020-08-15 15:10:07 +0200

    css geordent en smooth scroll toegevoegd

--- js/index.js
+++ js/index.js
@@ -9,3 +9,10 @@ var app = new Vue({
 gsap.to("h2.title", {duration: 1, opacity: 0.3});
 
 
+ $(function() {
+            $("html, body, *").mousewheel(function(event, delta) { 
+                this.scrollLeft -= (delta * 100);
+                this.scrollRight -= (delta * 100);
+                event.preventDefault();
+            });
+        });
